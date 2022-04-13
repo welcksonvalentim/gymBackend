32e4c5b4e2c7fcb076d45bc11b6faf8aac90b51f
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-13 11:09:36 -0300

    welckson valenitm - build: cria arquivo index.js e instala pacotes necessários para o projeto

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config();
+
+const cors = require('cors');
+
+const rescue = require('express-rescue');
+const studentController = require('./controllers/student');
+const errorMiddleware = require('./middlewares/error');
+const app = express();
+
+const { getStudent } = studentController;
+
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*' )
+    app.use(cors());
+    next();
+})
+const PORT = process.env.PORT || 3000;
+
+app.get('/student', rescue(getAllCars));
+
+app.use(errorMiddleware); 
+
+app.listen(PORT, () => console.log(`Executando na porta: ${PORT}`));
