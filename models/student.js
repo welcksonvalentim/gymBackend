dd71f59ce011350039954e951d361ca856b71954
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-14 07:49:54 -0300

    welckson valentim - feat: cria arquivo .env

--- models/student.js
+++ models/student.js
@@ -1,16 +1,21 @@
 const Student = (sequelize, DataTypes) => {
-    const Student = sequelize.define("Student", {
-      automaker: DataTypes.STRING,
-      model: DataTypes.STRING,
-      version: DataTypes.STRING,
-      image: DataTypes.STRING,
-      info: DataTypes.STRING
-    },
-    {
-      sequelize,
-      modelName: 'Student',
-    });
-    return Student;
-  };
+  const Student = sequelize.define("Student", {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+    age: DataTypes.NUMBER,
+    monday_practice: DataTypes.STRING,
+    thursday_practice: DataTypes.STRING,
+    wednesday_practice: DataTypes.STRING,
+    tuesday_practice: DataTypes.STRING,
+    friday_practice: DataTypes.STRING
+  },
+  {
+    sequelize,
+    modelName: 'Student',
+  });
+  return Student;
+};
   
-  module.exports = Student;
+module.exports = Student;
