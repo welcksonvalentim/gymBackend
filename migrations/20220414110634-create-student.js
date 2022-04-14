3be28190ad48ab9ecb56ba1eff9821c33ab66a5d
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-14 09:18:22 -0300

    welckson valentim - feat: cria arquivo migrations e insere informações

---
+++ migrations/20220414110634-create-student.js
@@ -0,0 +1,64 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      age: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      monday_practice: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      thursday_practice: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      wednesday_practice: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      tuesday_practice: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      friday_practice: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('students');
+  }
+};
---
+++ models/student.js
@@ -0,0 +1,22 @@
+'use strict';
+const Student = (sequelize, DataTypes) => {
+  const Student = sequelize.define("Student", {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+    age: DataTypes.NUMBER,
+    monday_practice: DataTypes.STRING,
+    thursday_practice: DataTypes.STRING,
+    wednesday_practice: DataTypes.STRING,
+    tuesday_practice: DataTypes.STRING,
+    friday_practice: DataTypes.STRING
+  },
+  {
+    sequelize,
+    modelName: 'Student',
+  });
+  return Student;
+};
+  
+module.exports = Student;
